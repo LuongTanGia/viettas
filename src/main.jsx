befb5f077919df94a28be90644bbdcca7c3a4f52
Tan Gia Luong <tangia123@outlook.com.vn>
2023-12-11 19:53:21 +0700

    fix clientID

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import store from "./redux/store.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <GoogleOAuthProvider
-        clientId="1087400588139-83e46v586sifjv3v8r5lbu8152nikkk9.apps.googleusercontent.com"
-        clientSecret="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"
-        callbackURL="/api/oauth/callback/google"
-      >
-        <App />
-      </GoogleOAuthProvider>
-    </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+        <React.StrictMode>
+            <GoogleOAuthProvider
+                clientId="587663293191-doosfu353ecu95jivrl4t0gas0dqf4sj.apps.googleusercontent.com"
+                clientSecret="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"
+                callbackURL="/api/oauth/callback/google"
+            >
+                <App />
+            </GoogleOAuthProvider>
+        </React.StrictMode>
+    </Provider>
 );
