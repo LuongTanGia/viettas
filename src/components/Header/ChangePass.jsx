920e97193d53b1e8e4ad973efaa05ed3829b232e
Tan Gia Luong <tangia123@outlook.com.vn>
2023-12-16 15:08:33 +0700

    fix_login

--- src/components/Header/ChangePass.jsx
+++ src/components/Header/ChangePass.jsx
@@ -1,128 +1,117 @@
-import { Button, Form, Input, Typography } from "antd";
-import { THAYDOIRMATKHAU } from "../../action/Actions";
-import API from "../../API/API";
+import { Form, Input, Typography } from 'antd'
+import { THAYDOIRMATKHAU } from '../../action/Actions'
+import API from '../../API/API'
 
 // eslint-disable-next-line react/prop-types
 function ChangePass({ isShow, close }) {
-    const onFinish = async (values) => {
-        const token = localStorage.getItem("TKN");
-        console.log("Success:", values);
-        await THAYDOIRMATKHAU(API.DOIMATKHAU, values, token);
-    };
-    const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
-        alert("Mật khẩu không trùng khớp !");
-    };
-    const { Title } = Typography;
-    return (
-        <>
-            {isShow ? (
-                <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center z-20">
-                    <div className="w-[600px] m-6 p-6 absolute shadow-lg bg-white rounded-md flex flex-col">
-                        <Title level={2}>Thay đổi mật khẩu </Title>
-                        <Form
-                            name="basic"
-                            className="ChangeLogin"
-                            labelCol={{
-                                span: 8,
-                            }}
-                            wrapperCol={{
-                                span: 16,
-                            }}
-                            style={{
-                                maxWidth: 600,
-                            }}
-                            initialValues={{
-                                remember: true,
-                            }}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item
-                                label="Mật Khẩu Hiện Tại"
-                                name="MatKhauHienTai"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Nhập mật khẩu hiện tại !",
-                                    },
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
+  const onFinish = async (values) => {
+    const token = localStorage.getItem('TKN')
+    console.log('Success:', values)
+    await THAYDOIRMATKHAU(API.DOIMATKHAU, values, token)
+    close()
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+    alert('Mật khẩu không trùng khớp !')
+  }
+  const { Title } = Typography
+  return (
+    <>
+      {isShow ? (
+        <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center z-20">
+          <div className="w-[600px] m-6 p-6 absolute shadow-lg bg-white rounded-md flex flex-col">
+            <Title level={2}>Thay đổi mật khẩu </Title>
+            <Form
+              name="basic"
+              className="ChangeLogin"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              style={{
+                maxWidth: 600,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Mật Khẩu Hiện Tại"
+                name="MatKhauHienTai"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Nhập mật khẩu hiện tại !',
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-                            <Form.Item
-                                label="Mật Khẩu Mới"
-                                name="MatKhauMoi"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Nhập mật khẩu cần thay đổi !",
-                                    },
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-                            <Form.Item
-                                label="Xác Mật Khẩu Mới"
-                                name="XacMatKhauMoi"
-                                dependencies={["MatKhauMoi"]}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message:
-                                            "Nhập lại mật khẩu cần thay đổi !",
-                                    },
-                                    ({ getFieldValue }) => ({
-                                        validator(_, value) {
-                                            if (
-                                                !value ||
-                                                getFieldValue("MatKhauMoi") ===
-                                                    value
-                                            ) {
-                                                return Promise.resolve();
-                                            }
-                                            return Promise.reject(
-                                                new Error(
-                                                    "Xác nhận mật khẩu không chính xác !"
-                                                )
-                                            );
-                                        },
-                                    }),
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-                            <Form.Item
-                                wrapperCol={{
-                                    offset: 8,
-                                    span: 16,
-                                }}
-                            >
-                                <Button
-                                    className="btn"
-                                    type="primary"
-                                    htmlType=""
-                                    onClick={() => {
-                                        close();
-                                    }}
-                                >
-                                    Hủy
-                                </Button>
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="btn"
-                                >
-                                    Thay đổi
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
+              <Form.Item
+                label="Mật Khẩu Mới"
+                name="MatKhauMoi"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Nhập mật khẩu cần thay đổi !',
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                label="Xác Mật Khẩu Mới"
+                name="XacMatKhauMoi"
+                dependencies={['MatKhauMoi']}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Nhập lại mật khẩu cần thay đổi !',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('MatKhauMoi') === value) {
+                        return Promise.resolve()
+                      }
+                      return Promise.reject(new Error('Xác nhận mật khẩu không chính xác !'))
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <div className="flex justify-end">
+                  <button
+                    className="active:scale-[.98] active:duration-75 text-white text-lg font-bold bg-rose-500 rounded-md px-2 py-1 w-[100px]"
+                    type=""
+                    onClick={() => {
+                      close()
+                    }}
+                  >
+                    Hủy
+                  </button>
+                  <button type="primary" className="active:scale-[.98] active:duration-75 text-white text-lg font-bold bg-blue-500 rounded-md px-2 py-1 w-[100px] ml-4">
+                    Thay đổi
+                  </button>
                 </div>
-            ) : null}
-        </>
-    );
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
 }
-export default ChangePass;
+export default ChangePass
